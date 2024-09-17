cff8b15c10b1a7e8ac33ef6de95970443df36b90
Katana-devel <egorpilipenko0@gmail.com>
2024-09-17 19:09:17 +0300

    Updated Home_Work1.py
    now its in right format. I did exclusion processing

--- Home_Work1.py
+++ Home_Work1.py
@@ -3,11 +3,14 @@ from datetime import datetime
 #func for counting days from today date
 def get_days_from_today(time_date):
 
-    converted_date = datetime.strptime(time_date, "%Y.%m.%d").date() #convertation
+    try:
+        converted_date = datetime.strptime(time_date, "%Y-%m-%d").date()
+    except ValueError:
+        converted_date = datetime.strptime(time_date, "%Y.%m.%d").date()
+
     today_date = datetime.now().date() #today date
     diff_days = today_date - converted_date #counting
 
     return diff_days.days
-
 
 print(get_days_from_today('2024.03.12'))
