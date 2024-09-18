b32e0302af1943b5484de8426774ff1eed869863
Katana-devel <egorpilipenko0@gmail.com>
2024-09-18 09:42:59 +0300

    Update Home_Work1.py

--- Home_Work1.py
+++ Home_Work1.py
@@ -1,16 +1,17 @@
 from datetime import datetime
 
+
 #func for counting days from today date
 def get_days_from_today(time_date):
-
     try:
         converted_date = datetime.strptime(time_date, "%Y-%m-%d").date()
+        today_date = datetime.now().date()
+        diff_days = today_date - converted_date
+        return diff_days
     except ValueError:
-        converted_date = datetime.strptime(time_date, "%Y.%m.%d").date()
+        print('Value i`snt correct')
+        return None
 
-    today_date = datetime.now().date() #today date
-    diff_days = today_date - converted_date #counting
 
-    return diff_days.days
-
-print(get_days_from_today('2024.03.12'))
+print(get_days_from_today('2024-3-12'))
+print(get_days_from_today("12.2.2000"))
