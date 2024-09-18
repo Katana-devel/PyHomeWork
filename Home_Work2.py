0d9daee4c2d4e7fe0eb3484cdf5d293c7220eeed
Katana-devel <egorpilipenko0@gmail.com>
2024-09-18 22:02:56 +0300

    Update Home_Work2.py

--- Home_Work2.py
+++ Home_Work2.py
@@ -2,6 +2,8 @@ from random import randint
 
 #Creat func for lottery tickets numbers
 def get_numbers_ticket(min, max, quantity):
+    if quantity > (max - min +1):
+        return []
     result = set()
     try:
         while len(result) != quantity:
@@ -11,4 +13,4 @@ def get_numbers_ticket(min, max, quantity):
     sorted_result = sorted(list(result))
     return sorted_result
 
-print(get_numbers_ticket(10, 4, 5))
+print(get_numbers_ticket(10, 14, 6))
